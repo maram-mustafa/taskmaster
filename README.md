42dc72eb779a82fd5fae8e20ba18e429a1d05333
maram-mustafa <maram.w.mustafa@gmail.com>
2021-08-30 05:50:35 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# TaskMaster
+
+## feature tasks
+
+1. Home page: this page containe a header, and there is an image in the center of the page, and it's contane two buttons (add task , all task) each button will guide to different page
+
+![homepage](./ScreenShots/Capture1.PNG)
+
+
+
+2. Add task Page: in this page there is a two plain text to enter the title and description for task, after fill the fields and click on add task it will show a msg submitted!
+
+![homepage](./ScreenShots/Capture2.PNG)
+
+
+3. All tasks page: this page contain all tasks, and the button home (to back to the home page)
+
+![homepage](./ScreenShots/Capture3.PNG)
--- app/src/main/java/com/example/lab1/AllTasks.java
+++ app/src/main/java/com/example/lab1/AllTasks.java
@@ -2,7 +2,10 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AllTasks extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class AllTasks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_tasks);
+
+        Button homebutton = findViewById(R.id.homebutton);
+        homebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToHome= new Intent(AllTasks.this,MainActivity.class);
+                startActivity(goToHome);
+            }
+        });
     }
 }
