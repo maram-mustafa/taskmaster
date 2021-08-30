fdda369a8d845dea049d262f0702f6d20799e528
maram-mustafa <maram.w.mustafa@gmail.com>
2021-08-30 05:37:21 +0300

    solve lab

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // get two buttons
+        Button addTasksButton = findViewById(R.id.button3);
+        Button allTasksButton = findViewById(R.id.button4);
+
+        // Add click listener
+        addTasksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                System.out.println("clicked");
+                // MOVE TO ANOTHER ACTIVITY
+                Intent goToAddTask= new Intent(MainActivity.this,AddTasks.class);
+                startActivity(goToAddTask);
+
+            }
+        });
+
+        // Add click listener
+        allTasksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // MOVE TO ANOTHER ACTIVITY
+                Intent goToAllTasks= new Intent(MainActivity.this,AllTasks.class);
+                startActivity(goToAllTasks);
+
+            }
+        });
+
     }
 }
